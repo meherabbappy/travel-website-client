4a51e77dd0ef0eef05135708b081bbda7d2ab54c
Meherab Hossain <60975003+meherabbappy@users.noreply.github.com>
2021-10-31 02:03:37 +0600

    added

--- src/Components/Home/Extra1/Extra1.js
+++ src/Components/Home/Extra1/Extra1.js
@@ -9,7 +9,7 @@ const Extra1 = () => {
 
             <div className="row row-cols-1 row-cols-md-4 g-4 w-75 m-auto">
 
-                <div className="col">
+                <div className="col col1">
                     <div className="card p-2 h-100">
                         <img src="https://livedemo00-joomla.template-help.com/joomla_prod-21623/templates/theme1588/images/home-icon1.png" className="card-img-top ccc" alt="..." />
                         <div className="card-body">
@@ -18,7 +18,7 @@ const Extra1 = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col col1">
                     <div className="card p-2 h-100">
                         <img src="https://livedemo00-joomla.template-help.com/joomla_prod-21623/templates/theme1588/images/home-icon2.png" className="card-img-top ccc" alt="..." />
                         <div className="card-body">
@@ -29,7 +29,7 @@ const Extra1 = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col col1">
                     <div className="card p-2 h-100">
                         <img src="https://livedemo00-joomla.template-help.com/joomla_prod-21623/templates/theme1588/images/home-icon3.png" className="card-img-top ccc" alt="..." />
                         <div className="card-body">
@@ -38,7 +38,7 @@ const Extra1 = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col col1">
                     <div className="card p-2 h-100">
                         <img src="https://livedemo00-joomla.template-help.com/joomla_prod-21623/templates/theme1588/images/home-icon4.png" className="card-img-top ccc" alt="..." />
                         <div className="card-body">
