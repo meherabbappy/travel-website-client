6365fa741494850ae934c33eb39d22ffb3e9f8d9
Meherab Hossain <60975003+meherabbappy@users.noreply.github.com>
2021-10-31 21:05:57 +0600

    added

--- src/Components/Allorders/Allorders.js
+++ src/Components/Allorders/Allorders.js
@@ -1,8 +1,94 @@
+// import React, { useEffect, useState } from 'react';
+// import { Row, Table } from 'react-bootstrap';
+// import Allorder from '../Allorder/Allorder';
+
+// const Allorders = () => {
+//     const [allorders, setAllorders] = useState([]);
+
+//     useEffect(() => {
+//         fetch('https://gentle-beach-61220.herokuapp.com/orders')
+//             .then(res => res.json()
+//                 .then(data => setAllorders(data)))
+//     }, [allorders])
+
+//     const handelDeleteOrder = id => {
+//         console.log(id);
+//         const proceed = window.confirm('Are you sure, you want to delete order?');
+//         if (proceed) {
+//             const url = `https://gentle-beach-61220.herokuapp.com/orders/${id}`;
+//             fetch(url, {
+//                 method: 'DELETE'
+//             })
+//                 .then(res => res.json())
+//                 .then(data => {
+//                     if (data.deletedCount > 0) {
+//                         alert('Deleted Successfully.');
+//                         const remainingOrders = allorders.filter(order => order._id !== id);
+//                         setAllorders(remainingOrders);
+//                     }
+//                 });
+//         }
+//     }
+//     //  All oder 
+
+//     const handelAcceptOrder = id => {
+//         const proceed = window.confirm('Are you sure, you want to Accept order?');
+//         if (proceed) {
+//             const url = `https://gentle-beach-61220.herokuapp.com/orders/${id}`;
+//             fetch(url, {
+//                 method: 'PUT',
+//                 headers: {
+//                     'content-type': 'application/json'
+//                 },
+//             })
+//                 .then(res => res.json())
+//                 .then(data => {
+//                     alert('Your Order Accepted');
+//                     setAllorders(allorders);
+//                 });
+//         }
+//     }
+
+
+
+//     return (
+//         <div>
+//             <div className="container pb-5">
+//                 <h2 className="p-2 fw-bold bg-dark text-white mt-4">All Orders</h2>
+//                 {/* Pass the data to another component using map  */}
+
+//                 <Table striped bordered hover size="sm" responsive>
+//                     <thead>
+//                         <tr>
+//                             <th>Order Id</th>
+//                             <th>Address</th>
+//                             <th>Status</th>
+//                             <th>Action</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         {
+//                             allorders.map(allorders => <Allorder
+//                                 handelDeleteOrder={handelDeleteOrder}
+//                                 allorders={allorders} key={allorders._id}></Allorder>)
+//                         }
+//                     </tbody>
+//                 </Table>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Allorders;
+
+
+
 import React, { useEffect, useState } from 'react';
-import { Row, Table } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import Allorder from '../Allorder/Allorder';
 
 const Allorders = () => {
+
     const [allorders, setAllorders] = useState([]);
 
     useEffect(() => {
@@ -29,12 +115,10 @@ const Allorders = () => {
                 });
         }
     }
-    //  All oder 
-
     const handelAcceptOrder = id => {
         const proceed = window.confirm('Are you sure, you want to Accept order?');
         if (proceed) {
-            const url = `http://localhost:5000/orders/${id}`;
+            const url = `https://gentle-beach-61220.herokuapp.com/orders/${id}`;
             fetch(url, {
                 method: 'PUT',
                 headers: {
@@ -43,25 +127,22 @@ const Allorders = () => {
             })
                 .then(res => res.json())
                 .then(data => {
-                    alert('Your Order Accepted');
+                    alert('Order Accepted');
                     setAllorders(allorders);
                 });
         }
     }
-
-
-
     return (
         <div>
             <div className="container pb-5">
-                <h2 className="p-2 fw-bold bg-dark text-white mt-4">All Orders</h2>
-                {/* Pass the data to another component using map  */}
+                <h1 className="p-2 fw-bold bg-dark text-white mt-4">All Orders</h1>
+                {/* Pass the data to another component using map */}
 
                 <Table striped bordered hover size="sm" responsive>
                     <thead>
                         <tr>
                             <th>Order Id</th>
-                            <th>Address</th>
+                            <th>Mail</th>
                             <th>Status</th>
                             <th>Action</th>
                         </tr>
@@ -69,6 +150,7 @@ const Allorders = () => {
                     <tbody>
                         {
                             allorders.map(allorders => <Allorder
+                                handelAcceptOrder={handelAcceptOrder}
                                 handelDeleteOrder={handelDeleteOrder}
                                 allorders={allorders} key={allorders._id}></Allorder>)
                         }
@@ -80,3 +162,5 @@ const Allorders = () => {
 };
 
 export default Allorders;
+
+
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Spinner, Row } from 'react-bootstrap';
 import Service from '../Service/Service';
 
 
@@ -10,19 +11,36 @@ const Services = () => {
             .then(data => setServices(data));
     }, [])
     return (
-        <div id="services" className="container">
-            <h2 className="text-dark fw-bold my-4 text-center">Our All Services</h2>
-            <div className="service-container">
-                <div className="row row-cols-1 row-cols-md-3 g-4">
-                    {
-                        services.map(service => <Service
-                            key={service.id}
-                            service={service}
-                        ></Service>)
-                    }
-                </div>
-            </div>
+        <div>
 
+
+            {services.length === 0 ?
+                <Button className="my-5 " variant="primary">
+                    <Spinner
+                        as="span"
+                        animation="grow"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                    />
+                    Loading...
+                </Button> :
+
+                <div id="services" className="container">
+                    <h2 className="text-dark fw-bold my-4 text-center">Our All Services</h2>
+                    <div className="service-container">
+                        <div className="row row-cols-1 row-cols-md-3 g-4">
+                            {
+                                services.map(service => <Service
+                                    key={service.id}
+                                    service={service}
+                                ></Service>)
+                            }
+                        </div>
+                    </div>
+
+                </div>
+            }
         </div>
     );
 };
